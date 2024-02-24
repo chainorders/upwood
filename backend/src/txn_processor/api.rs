5c0c28b04b52ecf69498fac2c10a1c6f0d699710
Parv Sharma <parv0888@gmail.com>
2024-02-25 04:17:49 +0530

    code reformat

--- backend/src/txn_processor/api.rs
+++ backend/src/txn_processor/api.rs
@@ -19,57 +19,83 @@ pub enum Error {
     InternalServerError,
 }
 impl From<mongodb::error::Error> for Error {
-    fn from(_: mongodb::error::Error) -> Self { Self::InternalServerError }
+    fn from(_: mongodb::error::Error) -> Self {
+        Self::InternalServerError
+    }
 }
 impl From<anyhow::Error> for Error {
-    fn from(_: anyhow::Error) -> Self { Self::InternalServerError }
+    fn from(_: anyhow::Error) -> Self {
+        Self::InternalServerError
+    }
 }
 impl From<AccountAddressParseError> for Error {
-    fn from(_: AccountAddressParseError) -> Self { Self::ParseError }
+    fn from(_: AccountAddressParseError) -> Self {
+        Self::ParseError
+    }
 }
 impl From<bson::ser::Error> for Error {
-    fn from(_: bson::ser::Error) -> Self { Self::ParseError }
+    fn from(_: bson::ser::Error) -> Self {
+        Self::ParseError
+    }
 }
 impl From<AddressParseError> for Error {
-    fn from(_: AddressParseError) -> Self { Self::ParseError }
+    fn from(_: AddressParseError) -> Self {
+        Self::ParseError
+    }
 }
 
-#[derive(Object)]
+#[derive(Object, Debug, Clone, Copy)]
 pub struct ApiContractAddress {
-    index:    u64,
+    index: u64,
     subindex: u64,
 }
+
+impl ApiContractAddress {
+    pub fn from_contract_address(contract_address: ContractAddress) -> Self {
+        Self {
+            index: contract_address.index,
+            subindex: contract_address.subindex,
+        }
+    }
+}
+
 impl From<DbContractAddress> for ApiContractAddress {
     fn from(value: DbContractAddress) -> Self {
         Self {
-            index:    value.0.index,
+            index: value.0.index,
             subindex: value.0.subindex,
         }
     }
 }
 impl From<ContractAddress> for ApiContractAddress {
     fn from(value: ContractAddress) -> Self {
-        Self {
-            index:    value.index,
-            subindex: value.subindex,
+        Self::from_contract_address(value)
+    }
+}
+
+impl From<ApiContractAddress> for ContractAddress {
+    fn from(val: ApiContractAddress) -> Self {
+        ContractAddress {
+            index: val.index,
+            subindex: val.subindex,
         }
     }
 }
 
-#[derive(Object)]
+#[derive(Object, Debug)]
 pub struct ApiAddress {
-    pub account_address:  Option<String>,
+    pub account_address: Option<String>,
     pub contract_address: Option<ApiContractAddress>,
 }
 impl From<DbAddress> for ApiAddress {
     fn from(value: DbAddress) -> Self {
         match value.0 {
             Address::Account(account_address) => Self {
-                account_address:  Some(account_address.to_string()),
+                account_address: Some(account_address.to_string()),
                 contract_address: None,
             },
             Address::Contract(contract_address) => Self {
-                account_address:  None,
+                account_address: None,
                 contract_address: Some(contract_address.into()),
             },
         }
@@ -79,6 +105,6 @@ impl From<DbAddress> for ApiAddress {
 #[derive(Object)]
 pub struct PagedResponse<T: Sync + Send + Type + ToJSON + ParseFromJSON> {
     pub page_count: u64,
-    pub page:       u64,
-    pub data:       Vec<T>,
+    pub page: u64,
+    pub data: Vec<T>,
 }
