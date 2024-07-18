e8cd92f65ced5534f2c8c788f16e401a5322bc50
Parv Sharma <parv0888@gmail.com>
2024-07-18 17:43:56 +0000

    backend dev container

--- .devcontainer/backend/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-rustup update \
-&& rustup toolchain install nightly \
-&& rustup component add rustfmt \
-&& rustup component add --toolchain nightly rustfmt \
-&& cargo install --locked cargo-concordium
-
-wget https://distribution.concordium.software/tools/linux/concordium-client_6.3.0-1 -O /usr/local/bin/concordium-client \
-&& chmod +x /usr/local/bin/concordium-client
