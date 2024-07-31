8055aa7b0982aa6949cf7cb95edab9ca58957b18
Parv Sharma <parv0888@gmail.com>
2024-08-01 04:59:46 +0530

    fixed backend dev container and added diesel cli

--- .devcontainer/backend/post-create.sh
+++ .devcontainer/backend/post-create.sh
@@ -1,9 +1,7 @@
 echo "Executing Post Create Commands"
-
 echo "Setting Yarn Version" \
 && corepack enable \
 && corepack install --global yarn@4.3.1 \
 && yarn set version stable \
-# && echo "\n Setting up Database" \
-# && cargo sqlx database setup \
+&& diesel database setup \
 && rustc --version
